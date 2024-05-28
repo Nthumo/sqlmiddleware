882e397547036a6c7a1d1ce728923ef0a55789a0
Nthumo <emmanuelmutinda235@gmail.com>
2024-05-28 19:55:28 +0300

    sqlmiddleware: middleware>2

--- sqlquerry/views.py
+++ sqlquerry/views.py
@@ -3,17 +3,10 @@ from .models import Product
 import json
 from django.http import JsonResponse
 from django.core.serializers import serialize
-from django.db import connection
-from sqlparse import format
 
 def home(request):
     qs = Product.objects.all()
     serialized_data = serialize('json', qs)
-
-    q = list(connection.queries)
-    for qs in q:
-        sqlformatted = format(str(qs["sql"]), reindent = True)  
-        print(sqlformatted)
     serialized_data = json.loads(serialized_data)
 
     return JsonResponse(serialized_data, safe = False, status=200)
