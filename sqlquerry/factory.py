340ced41a62fb5345fe8ef1715f835573e217804
Nthumo <emmanuelmutinda235@gmail.com>
2024-05-28 15:05:37 +0300

    sqlmiddleware: factoryboy-data

--- sqlquerry/factory.py
+++ sqlquerry/factory.py
@@ -7,3 +7,7 @@ FAKE = faker.Faker()
 class ProductFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Product
+
+    name = factory.Faker("sentence", nb_words = 12)
+    slug = factory.Faker("slug")
+    is_digital = False
--- sqlquerry/models.py
+++ sqlquerry/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Product(models.Model):
-    name = models.CharField(max_length = 100)
+    name = models.CharField(max_length = 200)
     slug = models.SlugField(max_length = 100)
     is_digital = models.BooleanField(default=False)
 
